67145cb20fa4780bbd849fd38687bf70d1930c95
dyabk <dzmitry.yabkevich@outlook.com>
2022-05-14 17:05:25 -0400

    Re-formatted the code with Prettier

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Header = (props) =>
-  <h1>{props.name}</h1>
-  
-const Content = (props) => {
-  const parts = props.parts
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
-const Part = (props) => (
-  <p>{props.name} {props.exercises}</p>
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
 )
 
-const Total = (props) => {
-  const parts = props.parts
-  return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  )
-}
-
 const App = () => {
-  // const-definitions
+  const [ counter, setCounter ] = useState(0)
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  /*setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )*/
   
+  const decreaseByZero = () => setCounter(counter - 1)
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  console.log('rendering...', counter)
+
   return (
     <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display counter={counter}/>
+      <Button onClick={decreaseByZero} text='minus' />
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={setToZero} text='zero' />
     </div>
   )
 }
